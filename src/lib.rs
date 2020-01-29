334bed3925fa28b8fb84135f402e15c873b8e0f4
Jan-Oliver 'Janonard' Opdenhövel <jan.opdenhoevel@protonmail.com>
2020-01-29 18:47:20 +0100

    Creating Capactitive

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,30 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use std::ops::*;
+
+pub trait Capacitive<I>: Index<I> + IndexMut<I> {
+    fn capacity(&self) -> I;
+}
+
+impl<T> Capacitive<usize> for [T] {
+    fn capacity(&self) -> usize {
+        self.len()
+    }
+}
+
+pub struct CapacityIter<'a, T> where T: Capacitive<usize> {
+    data: &'a T,
+    index: usize,
+}
+
+impl<'a, T> Iterator for CapacityIter<'a, T> where T: Capacitive<usize> + Index<usize> {
+    type Item = &'a T::Output;
+
+    fn next(&mut self) -> Option<&'a T::Output> {
+        if self.index < self.data.capacity() {
+            let item = &self.data[self.index];
+            self.index += 1;
+            Some(item)
+        } else {
+            None
+        }
     }
 }
