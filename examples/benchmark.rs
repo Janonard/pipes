fb8a3290c5b32dc94369f437ff7a6544c648ee67
Jan-Oliver 'Janonard' Opdenhövel <jan.opdenhoevel@protonmail.com>
2020-03-30 13:53:37 +0200

    Adding statistics to the benchmark

--- examples/benchmark.rs
+++ examples/benchmark.rs
@@ -1,5 +1,5 @@
 use pipes::*;
-use time::{Duration, Instant};
+use time::Instant;
 
 mod piped {
     use pipes::Pipe;
@@ -136,13 +136,13 @@ mod manual {
     }
 }
 
-fn benchmark_pipe<P, F>(length: usize, runs: usize, mut factory: F)
+fn benchmark_pipe<P, F>(length: usize, runs: usize, mut factory: F) -> Vec<f32>
 where
     P: Pipe<InputItem = usize, OutputItem = f32>,
     F: FnMut() -> P,
 {
     let mut signal_buffer = vec![0.0; length].into_boxed_slice();
-    let mut durations: Vec<Duration> = Vec::with_capacity(runs);
+    let mut durations: Vec<f32> = Vec::with_capacity(runs);
 
     for _ in 0..runs {
         let mut pipe = factory();
@@ -151,41 +151,102 @@ where
             signal_buffer[i] = pipe.next(i);
         }
         let end = Instant::now();
-        let duration = end - start;
-        print!("{}, ", duration.as_seconds_f32());
+        let duration = (end - start).as_seconds_f32();
+        print!("{}, ", duration);
         durations.push(duration);
     }
     println!();
 
-    let durations: Vec<f32> = durations
-        .iter()
-        .map(|duration| duration.as_seconds_f32())
-        .collect();
-    println!(
-        "# Mean: {}",
-        durations.iter().cloned().sum::<f32>() / runs as f32
-    );
+    durations
 }
+
+const INFO: &str = "# This program benchmarks pipes by rendering a simple metronome signal.
+# The signal is calculated by a pipes-based implementation first and by a manually implementated one
+# afterwards. Both implementations are executed 200 times each, which will take about 15 minutes,
+# depending on your system. The runtime of each execution is printed in CSV-style format, which can
+# parsed and analyzed.
+#
+# This benchmark shows that pipes-based implementations aren't significantly slower than manually
+# implemented ones; Only by 0.7 +- 1.2%.
+#
+";
 
 fn main() {
     const LEN: usize = 200_000_000;
-    const RUNS: usize = 10;
+    const RUNS: usize = 200;
 
-    println!("# This program benchmarks a simple metronome,");
-    println!("# once implemented with pipes and once implemented manually.");
-    println!("# The individual runtimes are printed as CSV.");
+    print!("{}", INFO);
 
     println!("# Runtimes of the piped version:");
     let mut counter: usize = 0;
-    benchmark_pipe(LEN, RUNS, move || {
+    let piped_durations: Vec<f32> = benchmark_pipe(LEN, RUNS, move || {
         counter += 1;
         piped::Metronome::new(500, 500, 100 * counter, 1_000)
     });
 
     println!("# Runtimes of the manual version:");
     let mut counter: usize = 0;
-    benchmark_pipe(LEN, RUNS, move || {
+    let manual_durations: Vec<f32> = benchmark_pipe(LEN, RUNS, move || {
         counter += 1;
         manual::Metronome::new(500, 500, 100 * counter, 1_000)
     });
+
+    let mean_duration_piped: f32 =
+        piped_durations.iter().sum::<f32>() / piped_durations.len() as f32;
+
+    let mean_duration_manual: f32 =
+        manual_durations.iter().sum::<f32>() / manual_durations.len() as f32;
+
+    let combined: Vec<(f32, f32)> = Iterator::zip(piped_durations.iter(), manual_durations.iter())
+        .map(|(piped, manual)| (piped - manual, piped / manual))
+        .collect();
+
+    let (
+        mean_difference,
+        mean_relation,
+        min_difference,
+        max_difference,
+        min_relation,
+        max_relation,
+    ) = combined.iter().cloned().fold(
+        (
+            0.0,
+            0.0,
+            combined[0].0,
+            combined[0].0,
+            combined[0].1,
+            combined[0].1,
+        ),
+        |(diff_sum, rel_sum, min_diff, max_diff, min_rel, max_rel), (diff, rel)| {
+            (
+                diff_sum + diff,
+                rel_sum + rel,
+                f32::min(min_diff, diff),
+                f32::max(max_diff, diff),
+                f32::min(min_rel, rel),
+                f32::max(max_rel, rel),
+            )
+        },
+    );
+    let (mean_difference, mean_relation) = (
+        mean_difference / combined.len() as f32,
+        mean_relation / combined.len() as f32,
+    );
+
+    println!(
+        "# Mean duration of a piped execution: {}s",
+        mean_duration_piped
+    );
+    println!(
+        "# Mean duration of a manual execution: {}s",
+        mean_duration_manual
+    );
+    println!(
+        "# Minimal, Mean, and Maximal difference between a piped and a manual execution: {}s, {}s, {}s",
+        min_difference, mean_difference, max_difference
+    );
+    println!(
+        "# Minimal, Mean, and Maximal relation between a piped and a manual exection: {}, {}, {}",
+        min_relation, mean_relation, max_relation
+    );
 }
